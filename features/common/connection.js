0f176ea1470350f948a5d3bc4a0c74806608f4ef
Vizorous <vikumwijekoon97@gmail.com>
2022-03-27 12:27:34 +0530

    added get url

--- features/common/connection.js
+++ features/common/connection.js
@@ -1,26 +1,33 @@
 var mysql = require("mysql2/promise");
 
-const axiosConfig = (status, lon, lat, direction) => ({
-  method: "post",
-  url: "http://192.168.1.2",
-  data: {
-    status: status,
-
-    lon: lon,
-    lat: lat,
-    direction: direction,
-  },
-});
-
 async function connection(sql, ...params) {
-  const con = await mysql.createConnection({
-    host: "localhost",
-    user: "user",
-    password: "Password123",
-    database: "test",
-  });
-  const [rows, fields] = await con.execute(sql, params);
-  return [rows, fields];
+	const con = await mysql.createConnection({
+		host: "localhost",
+		user: "user",
+		password: "Password123",
+		database: "test",
+	});
+	const [rows, fields] = await con.execute(sql, params);
+	return [rows, fields];
 }
+
+async function getURL() {
+	const [rows, fields] = await connection("SELECT * FROM `server`");
+	const respObj = rows[0];
+	console.log(respObj);
+	return respObj;
+}
+getURL();
+const axiosConfig = (status, lon, lat, direction) => ({
+	method: "post",
+	url: "http://192.168.1.2",
+	data: {
+		status: status,
+
+		lon: lon,
+		lat: lat,
+		direction: direction,
+	},
+});
 
 module.exports = { connection, axiosConfig };
